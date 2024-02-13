7cc3061e6a608e2fcc98553860fe7208d909e255
Debraj Banik <writetodebrajbanik@gmail.com>
2024-02-13 12:45:25 +0530

    added es-lint

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,6 +30,7 @@ const Header = () => {
     });
     //Unsubscribe when component unmounts!
     return () => unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex w-full justify-between">
